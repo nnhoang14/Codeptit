f0b9cd6389706fbcf3583a0d528ee95c2e179c4d
nhhoang.14 <45280327+nnhoang14@users.noreply.github.com>
2024-04-19 19:03:11 +0700

    Create C01050.c

---
+++ C/C01050.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    for(int i=1;i<=b;i++){
+        for(int j=1;j<=a;j++){
+            if(i==1||i==b)printf("*");
+            else{
+                if(j==1||j==a)printf("*");
+                else printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
