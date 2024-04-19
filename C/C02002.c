66219b8c0d9793ebd450e9dc5e779f63d5bb9734
nhhoang.14 <45280327+nnhoang14@users.noreply.github.com>
2024-04-19 19:04:57 +0700

    Create C02002.c

---
+++ C/C02002.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d",&a);
+    b=a-1;
+    for(int i=1;i<=a;i++){
+        for(int m=1;m<=b;m++){
+            printf("~");
+        }
+        for(int j=1;j<=a;j++){
+            printf("*");
+        }
+        printf("\n");
+        b--;
+    }
+    return 0;
+}
