aac8b54371dc6866c81c95fe517fbb412d464860
nhhoang.14 <45280327+nnhoang14@users.noreply.github.com>
2024-04-19 19:27:51 +0700

    Create C04024.c

---
+++ C/C04024.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int n,a[100];
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)scanf("%d",&a[i]);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(a[i]>a[j]){
+                int tg=a[i];
+                a[i]=a[j];
+                a[j]=tg;
+            }
+        }
+    }
+    for(int i=0;i<n;i++)printf("%d ",a[i]);
+    return 0;
+}
