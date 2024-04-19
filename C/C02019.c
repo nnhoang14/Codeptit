f04b6dcbe99e10cbeba774be17be7d8f670b0c6b
nhhoang.14 <45280327+nnhoang14@users.noreply.github.com>
2024-04-19 19:33:50 +0700

    Create C02019.c

---
+++ C/C02019.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        int kt=i*2-1,tmp=kt+1;
+        for(int j=1;j<=kt;j++){
+            int s=j*2;
+            if(s<=kt+1)printf("%d",s);
+            else {
+                tmp-=2;
+                printf("%d",tmp);
+            }
+        }
+        printf("\n");
+    }
+}
