c4815086980def6e6a9ebb20305c19875e5a36b4
nhhoang.14 <45280327+nnhoang14@users.noreply.github.com>
2024-04-19 19:25:05 +0700

    Create C04014.c

---
+++ C/C04014.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,a[100],b[100]={0};
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+        b[a[i]]++;
+    }
+    for(int i=0;i<n;i++){
+        if(b[a[i]]>0){
+            printf("%d %d\n",a[i],b[a[i]]);
+            b[a[i]]=0;
+        }
+    }
+    return 0;
+}
