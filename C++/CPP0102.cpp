0e05820660f93830b78f1012288f441eb59887c0
nhhoang.14 <45280327+nnhoang14@users.noreply.github.com>
2024-08-14 00:35:27 +0700

    Update CPP0102.cpp

--- C++/CPP0102.cpp
+++ C++/CPP0102.cpp
@@ -1,3 +1,16 @@
+/*Cho một chữ cái bất kỳ. Hãy kiểm tra xem đó là chữ hoa hay chữ thường. Nếu là chữ thường thì in ra chữ hoa, nếu là chữ hoa thì in ra chữ thường tương ứng. 
+Input
+Dòng đầu ghi số bộ test. Mỗi bộ test có duy nhất một chữ cái.
+Output
+Với mỗi bộ test, ghi ra kết quả trên một dòng.
+
+Ví dụ
+Input            Output
+2                b
+B                Q
+q
+*/
+
 #include <iostream>
 using namespace std;
 
