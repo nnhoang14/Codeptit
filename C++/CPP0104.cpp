8f6ae1da26a843ccb7cabd12462f8aa90885c34c
nhhoang.14 <45280327+nnhoang14@users.noreply.github.com>
2024-08-17 22:38:12 +0700

    Add files via upload

---
+++ C++/CPP0104.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int n;
+    long long sum=1,tmp=1;
+    cin>>n;
+    for(int i = 2; i <= n; i++){
+        tmp=tmp*i;
+        sum+=tmp;
+    }
+    cout<<sum;
+    return 0;
+}
