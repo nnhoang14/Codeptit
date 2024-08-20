dcde3f191bb61f5ca2566eec7df284c202caf6e8
nhhoang.14 <45280327+nnhoang14@users.noreply.github.com>
2024-08-20 19:27:06 +0700

    Add files via upload

---
+++ C++/CPP0109.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int check_cl(int n){
+    int c=0,l=0,a;
+    while(n>0){
+        a=n%10;
+        if(a%2==0)c++;
+        else l++;
+        n/=10;
+    }
+    if(c==l)return 1;
+    else return 0;
+}
+
+int main() 
+{
+    int n,count=0;
+    cin>>n;
+    for(int i = pow(10,n-1); i < pow(10,n); i++){
+        if(check_cl(i)){
+            cout<<i<<" ";
+            count++;
+            }
+        if(count == 10){
+            cout<<"\n";
+            count = 0;
+            }
+    }
+    return 0;
+}
