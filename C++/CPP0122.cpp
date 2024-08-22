228e9e80a19821a62bcd9435a78ca50723f5db60
nhhoang.14 <45280327+nnhoang14@users.noreply.github.com>
2024-08-22 21:22:21 +0700

    Add files via upload

---
+++ C++/CPP0122.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int is_prime(int num){
+    if(num<2)return 0;
+    for(int i = 2; i <= sqrt(num); i ++){
+        if(num%i==0)return 0;
+    }
+    return 1;
+}
+
+int main() 
+{
+    int test;
+    cin>>test;
+    while (test--)
+    {
+        int num;
+        cin>>num;
+        int a[num];
+        fill(a,a+num,1);
+        long long res=1;
+        for(int i = 0; i < num; i++){
+            if(is_prime(i+1)){
+                for(int j=i+1; j <= num; j*=(i+1))
+                    a[j-1]=i+1;
+            }
+        }
+        for(int i = 0; i < num; i++)
+            res*=a[i];
+        cout<<res<<endl;
+    }
+    return 0;
+}
---
+++ C++/CPP0123.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int is_prime(int num){
+    if(num<2)return 0;
+    for(int i = 2; i <= sqrt(num); i ++){
+        if(num%i==0)return 0;
+    }
+    return 1;
+}
+
+int main() 
+{
+    int num;
+    cin>>num;
+    if(is_prime(num))cout<<"YES";
+    else cout<<"NO";
+}
