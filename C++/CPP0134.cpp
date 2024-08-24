efb8fcb4ce7729c1e313283a042e1148803dfd92
nhhoang.14 <45280327+nnhoang14@users.noreply.github.com>
2024-08-24 23:23:22 +0700

    Add files via upload

---
+++ C++/CPP0134.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(int num, int k){
+    int count=0;
+    for(int i = 2; i <= sqrt(num); i++){
+           while (num%i==0){
+            count++;
+            if(count==k)return i;
+            num/=i;
+           }
+        }
+        if(num>1&&count==k-1)return num;
+        else return -1;
+}
+
+int main() {
+    int test;
+    cin>>test;
+    while (test--)
+    {
+        int num,k,res;
+        cin>>num>>k;
+        cout<<solve(num,k)<<endl;
+    }
+    return 0;
+}
