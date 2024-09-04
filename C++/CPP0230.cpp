569726f8b7a3e8fdf422edbc4decd1ed7b4db63c
nhhoang.14 <45280327+nnhoang14@users.noreply.github.com>
2024-09-04 23:15:57 +0700

    Create CPP0230.cpp

---
+++ C++/CPP0230.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int num,count=0;
+    cin>>num;
+    vector<vector<int>>matrix(num,vector<int>(3));
+    for(int i = 0; i < num; i++){
+      int dem=0;
+      for(int j = 0; j < 3; j++){
+        cin>>matrix[i][j];
+        if(matrix[i][j]==1)dem++;
+      }
+      if(dem>1)count++;
+    }
+    cout<<count;
+    return 0;
+}
