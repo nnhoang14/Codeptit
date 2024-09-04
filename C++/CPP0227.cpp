b655ed1a796b2060720cce23d529bf10379b6485
nhhoang.14 <45280327+nnhoang14@users.noreply.github.com>
2024-09-04 23:15:18 +0700

    Create CPP0227.cpp

---
+++ C++/CPP0227.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int test;
+    cin>>test;
+    while(test--){
+      int num;
+      cin>>num;
+      vector<vector<int>>matrix(num,vector<int>(num));
+      for(int i = 0; i < num; i++){
+        for(int j = 0; j < num; j++){
+          cin>>matrix[i][j];
+        }
+      }
+      for(int i = 0; i < num; i++){
+        int pt=num-1;
+        for(int j = 0; j < num; j++){
+          if(i%2==0)cout<<matrix[i][j]<<" ";
+          else{
+            cout<<matrix[i][pt]<<" ";
+            pt--;
+          }
+        }
+      }
+      cout<<endl;
+    }
+    return 0;
+}
