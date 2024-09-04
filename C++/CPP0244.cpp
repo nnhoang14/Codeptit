bb0d1b2586fb18fdd3cce24dde88a4d41c43ed3c
nhhoang.14 <45280327+nnhoang14@users.noreply.github.com>
2024-09-04 23:16:26 +0700

    Create CPP0244.cpp

---
+++ C++/CPP0244.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int num;
+    cin>>num;
+    vector<int> matrix(num);
+    vector<int> res;
+    for(int i = 0; i < num; i++)
+      cin>>matrix[i];
+    res.push_back(matrix[0]); 
+    for(int i = 1; i < num; i++){
+      bool found = false;
+      for(int j = 0; j < res.size(); j++){
+        if(matrix[i]==res[j]){
+          found = true;
+          break;
+        }
+      }
+      if(!found)res.push_back(matrix[i]);
+    }
+    sort(res.begin(),res.end());
+    for(int i = 0; i < res.size(); i++)
+      cout<<res[i]<<" ";
+    return 0;
+}
