e41455b3af97e900c090329b83b6b4aacc3d1f00
nhhoang.14 <45280327+nnhoang14@users.noreply.github.com>
2024-09-04 23:14:38 +0700

    Create CPP0220.cpp

---
+++ C++/CPP0220.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int test;
+    cin>>test;
+    while(test--){
+      int n;
+      cin>>n;
+      vector<vector<int>>matrix(n,vector<int>(n));
+      for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+          cin>>matrix[i][j];
+        }
+      }
+      
+      for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+          if (i == 0 || i == n - 1 || j == 0 || j == n - 1) cout<<matrix[i][j]<<" ";
+          else cout<<"  ";
+        }
+        cout<<endl;
+      }
+    }
+    return 0;
+}
