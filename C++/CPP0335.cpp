11e18178acc1a0187a42283051e3a8e8eb9c1601
nhhoang.14 <45280327+nnhoang14@users.noreply.github.com>
2024-11-14 19:20:41 +0700

    Add files via upload

---
+++ C++/CPP0335.cpp
@@ -0,0 +1,32 @@
+//https://code.ptit.edu.vn/student/question/CPP0335
+//TÌM SỐ LỚN NHẤT TRONG XÂU
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+    int test;
+    cin >> test;
+    cin.ignore();
+    while (test--){
+        string s; 
+        cin >> s;
+        int tmp = 0;
+        vector<int> res;
+        for(int i = 0; i < s.length(); i++){
+            if(isdigit(s[i])){
+                int num = s[i] - '0';
+                tmp = tmp*10 + num;
+            }
+            else{
+                res.push_back(tmp);
+                tmp = 0;
+            }
+        }
+        if(tmp != 0 )res.push_back(tmp);
+        sort(res.begin(),res.end());
+        cout << res[res.size() - 1] << endl;
+    }
+    return 0;
+}
