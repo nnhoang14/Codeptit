e3ca6e6ff56cb3bcb27c7e86c6f4c4ff55ed2b8e
nhhoang.14 <nhhoangne@gmail.com>
2024-11-23 16:37:06 +0700

    Add files via upload

---
+++ C++/CPP0621.cpp
@@ -0,0 +1,51 @@
+//https://code.ptit.edu.vn/student/question/CPP0621
+//SẮP XẾP THEO MÃ SINH VIÊN
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class SinhVien{
+    private:
+        string msv;
+        string name;
+        string cl;
+        string email;
+    public:
+        SinhVien();
+        friend istream &operator >> (istream &in, SinhVien &ds);
+        friend ostream &operator << (ostream &out, SinhVien ds);
+        string getmsv(){ return msv; }
+};
+
+SinhVien::SinhVien(){
+    msv = name = cl = email = "";
+}
+
+istream &operator >> (istream &in, SinhVien &ds){
+    cin >> ws;
+    getline(cin, ds.msv);
+    getline(cin, ds.name);
+    getline(cin, ds.cl);
+    getline(cin, ds.email);
+    return in;
+}
+
+ostream &operator << (ostream &out, SinhVien ds){
+    out << ds.msv << " " << ds.name << " " << ds.cl << " " << ds.email << endl;
+    return out;
+}
+
+bool cmp(SinhVien a, SinhVien b){
+    return a.getmsv() < b.getmsv();
+}
+
+int main(){
+    SinhVien a;
+    vector<SinhVien> ds;
+    while(cin >> a){
+        ds.push_back(a);
+    }
+    sort(ds.begin(), ds.end(), cmp);
+    for(auto x : ds) cout << x;
+    return 0;
+}
