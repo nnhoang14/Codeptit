24848ba476c6a6aea1819a0910e33e4929e19d8b
nhhoang.14 <nhhoangne@gmail.com>
2024-11-29 14:10:12 +0700

    Update CPP0262.cpp

--- C++/CPP0262.cpp
+++ C++/CPP0262.cpp
@@ -15,11 +15,12 @@ int main() {
         ll n, m; 
         cin >> n >> m;
         ll sum = n*(n+1)/2;
+        if((sum - m) % 2 != 0 || (sum - m) < 0){
+            cout << "No" << endl;
+            continue;
+        }
         ll t2 = (sum - m)/2;
         ll t1 = t2 + m;
-        cout << t1 << " " << t2 << " ";
-        /* if(isPrime(t2)) cout << "t2" << endl;
-        if(isPrime(t1)) cout << "t1" << endl; */
         if(check(t1,t2)) cout << "Yes" << endl;
         else cout << "No" << endl;
     }
