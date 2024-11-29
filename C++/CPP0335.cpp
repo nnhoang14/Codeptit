bca675cddbcdde7ec66c4d2896d97c5ddf75d738
nhhoang.14 <nhhoangne@gmail.com>
2024-11-29 09:32:40 +0700

    Update CPP0335.cpp

--- C++/CPP0335.cpp
+++ C++/CPP0335.cpp
@@ -5,38 +5,27 @@
 using namespace std;
 typedef long long ll;
 
-int same_num(string s) {
-    int n = s.length() - 1;
-    return (s[n - 5] == s[n - 4] && s[n - 4] == s[n - 3] && s[n - 1] == s[n]);
-}
-
-int all_six_eight(string s){
-    int n = s.length() - 1;
-    for(int i = n - 5; i <= n; i++){
-        if(i == n - 2) continue;
-        if(s[i] != '6' && s[i] != '8') return 0;
-    }
-    return 1;
-}
-
-int ascending(string s){
-    int n = s.length() - 1;
-    for(int i = n - 5; i < n; i++){
-        if(i != n - 3 && s[i] >= s[i + 1]) return 0;
-        else if (i == n- 3 && s[i] >= s[i + 2]) return 0;
-    }
-    return 1;
-}
-
 int main(){
     int test;
     cin >> test;
     cin.ignore();
     while (test--){
         string s; 
-        getline(cin, s);
-        if(same_num(s) || all_six_eight(s) || ascending(s)) cout << "YES" << endl;
-        else cout << "NO" << endl;
+        cin >> s;
+        int tmp = 0;
+        vector<int> res;
+        for(int i = 0; i < s.length(); i++){
+            if(isdigit(s[i])){
+                int num = s[i] - '0';
+                tmp = tmp*10 + num;
+            }
+            else{
+                res.push_back(tmp);
+                tmp = 0;
+            }
+        }
+        if(tmp != 0 )res.push_back(tmp);
+        sort(res.begin(),res.end());
+        cout << res[res.size() - 1] << endl;
     }
     return 0;
-}
