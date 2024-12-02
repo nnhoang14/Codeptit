fddc7c811847fb4b783e00137f5568148c83c861
nhhoang.14 <nhhoangne@gmail.com>
2024-12-02 22:46:06 +0700

    Add files via upload

--- C++/CPP0338.cpp
+++ C++/CPP0338.cpp
@@ -1,32 +1,29 @@
-//https://code.ptit.edu.vn/student/question/CPP0338
-//ĐẾM XÂU CON
+//https://code.ptit.edu.vn/student/question/CPP0429
+//SỬA ĐÈN
 
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
-int dxc(string s, int k){
-    int cnt = 0, lm = s.length();
-    for(int i = 0; i < lm; i ++){
-        set<char> kt;
-        for(int j = i; j < lm; j++){
-            kt.insert(s[j]);
-            if(kt.size() == k) cnt++;
-            if(kt.size() > k) break;
-        }
-    }
-    return cnt;
-}
-
 int main() {
-	int test;
-    cin >> test;
-    cin.ignore();
-    while (test--){
-        int k;
-        string s;
-        cin >> s >> k;
-        cout << dxc(s,k) << endl;
+	int n, k, b;
+    cin >> n >> k >> b;
+    vector<int> ds(n,1);
+    for(int i = 0; i < b; i++){
+        int tmp;
+        cin >> tmp;
+        ds[tmp - 1] = 0;
     }
+    int broken = 0;
+    for(int i = 0; i < k; i++){
+        if(ds[i] == 0)broken ++;
+    }
+    int fix = broken;
+    for(int i = k; i < n; i++){
+        if(ds[i] == 0)broken ++;
+        if(ds[i - k] == 0)broken --;
+        fix = min(fix, broken);
+    }
+    cout << fix;
 	return 0;
 }
