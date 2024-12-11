60ccf685acc90850dbdb40780ee472e0ed569e2f
nhhoang.14 <nhhoangne@gmail.com>
2024-12-11 17:43:07 +0700

    Add files via upload

--- C++/CPP0242.cpp
+++ C++/CPP0242.cpp
@@ -1,30 +1,34 @@
-//https://code.ptit.edu.vn/student/question/CPP0242
-//DÃY SỐ NHỊ PHÂN
+// https://code.ptit.edu.vn/student/question/CPP0261
+// CỬA SỔ TRƯỢT
 
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
-int main() {
-	int test;
-    cin >> test;
-    while (test--){
-        int n;
-        cin >> n;
-        int a[n];
-        int b[n];
-        for(auto &x : a) cin >> x;
-        for(auto &x : b) cin >> x;
-        int lm = 1;
-        for(int i = 0; i < n; i++){
-            int s1 = 0, s2 = 0; 
-            for(int j = i; j < n; j++){
-                s1 += a[j];
-                s2 += b[j];
-                if(s1 == s2) lm = max(lm, j - i + 1);
-            }
+int main(){
+    int n, m;
+    cin >> n;
+    int a[n][n];
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            cin >> a[i][j];
         }
-        cout << lm << endl;
     }
-	return 0;
+    cin >> m;
+    int b[m][m];
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < m; j++){
+            cin >> b[i][j];
+        }
+    }
+    if (n % m == 0){
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                a[i][j] *= b[i % m][j % m];
+                cout << a[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+    return 0;
 }
