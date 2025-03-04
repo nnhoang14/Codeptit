05982c8891b0041fb391beb7e1cedb8bd3a69eca
nhhoang14 <nhhoangne@gmail.com>
2025-03-04 15:58:52 +0700

    Add DSA/CTDL_005.cpp

---
+++ DSA/CTDL_005.cpp
@@ -0,0 +1,25 @@
+//https://code.ptit.edu.vn/beta/problems/CTDL_005
+//XÓA DỮ LIỆU TRONG DSLK ĐƠN
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+};
+
+typedef struct Node* node;
+
+node makeNode(int x){
+    node tmp = new Node();
+    tmp -> data = x;
+    tmp -> next = NULL;
+    return tmp;
+}
+
+int main(){
+    node head = new Node();
+    
+    return 0;
+}
